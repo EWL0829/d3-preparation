b09bbc08cfdebf2d915a790e1a9fc9ed9ed806ec
liyue <li.yue@okcoin.com>
2019-03-24 21:56:08 +0800

    添加基础环状图

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SortableBarChart from './sortableBarChart';
-import BasicExample from './basicExample';
+
+// examples
+import SortableBarChart from './sortableBarChart/sortableBarChart';
+import BasicExample from './basicExamples/basicExample';
+import ArcPadding from './arcPadding-I/arcPadding';
+
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<SortableBarChart />, document.getElementById('root'));
-ReactDOM.render(<BasicExample />, document.getElementById('root'));
+ReactDOM.render(<ArcPadding />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
