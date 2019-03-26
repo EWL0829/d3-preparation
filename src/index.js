64254bee63c85bfedd1bef994c9de6bc62b8a3a5
liyue <li.yue@okcoin.com>
2019-03-26 14:07:40 +0800

    添加悬浮时的弹性过渡

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import './index.css';
 // examples
 import SortableBarChart from './sortableBarChart/sortableBarChart';
 import BasicExample from './basicExamples/basicExample';
-// import ArcPadding from './arcPadding-I/arcPadding';
+import ArcPadding from './arcPadding-I/arcPadding';
 // import ArcPadding from './arcPadding-II/arcPadding';
 // import ArcPadding from './arcPadding-III/arcPadding';
 // import ArcPadding from './arcPadding-IV/arcPadding';
-// import ExtendingArc from './extendingArc/extendingArc';
+import ExtendingArc from './extendingArc/extendingArc';
 import GroupedBar from './groupedBar/groupedBar';
 
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<SortableBarChart />, document.getElementById('root'));
-ReactDOM.render(<GroupedBar />, document.getElementById('root'));
+ReactDOM.render(<ExtendingArc />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
